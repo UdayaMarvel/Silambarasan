ae36dda9b099457072d583fbed8763260228dd7e
UdayaMarvel <udayamarvel@gmail.com>
2023-12-07 20:35:21 +0530

    First Commit

---
+++ src/Practice/FirstClass.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class FirstClass {
+	
+public void rameshRam(){
+	System.out.println("Ramesh");
+
+}
+public void silambuArasan(){
+	System.out.println("Silambarasan");
+}
+
+public static void main(String[] args) {
+	FirstClass cl = new FirstClass();
+	cl.silambuArasan();
+	cl.rameshRam();
+
+}
+}
